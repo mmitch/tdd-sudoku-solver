b2655b8b474871a2584c188e10528d77d732e31a
Christian Garbs <mitch@cgarbs.de>
2016-08-13 22:23:46 +0200

    add missing Game interface

---
+++ src/main/java/Game.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Game {
+
+	public static final Collection<Integer> ALL_NUMBERS = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+}
