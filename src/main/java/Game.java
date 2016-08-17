f87a455d31ecf650d501b9b7adbe269f90496414
Christian Garbs <mitch@cgarbs.de>
2016-08-18 00:52:03 +0200

    Game is an interface
    no class needed

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 import java.util.Collection;
 
-public class Game {
+public interface Game {
 
-	public final static int ROWS = 9;
-	public final static int COLS = 9;
-	public final static int BLOCK_ROWS = 3;
-	public final static int BLOCK_COLS = 3;
-	public static final Collection<Integer> ALL_NUMBERS = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+	int ROWS = 9;
+	int COLS = 9;
+	int BLOCK_ROWS = 3;
+	int BLOCK_COLS = 3;
+	Collection<Integer> ALL_NUMBERS = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 }
